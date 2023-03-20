7ea81d4aae3511fa27c601908c9b2a7fd351608d
PHKhande <phk151raj@gmail.com>
2023-03-20 22:00:55 +0530

    Sib Integration

---
+++ password/forgotpassword.js
@@ -0,0 +1,36 @@
+const btnReset = document.getElementById("btnLogin");
+btnReset.addEventListener("click", validateFunc);
+
+function validateFunc(e) {
+    const ResetEmail = document.getElementById("emailId").value;
+    
+    if (ResetEmail == "") {
+      alert("Email must be filled out");
+      return false;
+    }
+
+    else{
+        forgotPassword(e);
+    }
+}
+
+async function forgotPassword(e){
+
+    try{
+        e.preventDefault();
+        const resetEmail = document.getElementById("emailId").value;
+    
+        const obj = {
+            resetEmail
+        }
+        const forgotEmail = await axios.post('http://localhost:3000/password/forgotpassword', obj);
+        window.location.href = "../login/login.html";
+           
+    }
+
+    catch (err) {
+        console.log(err);
+        document.body.innerHTML += `<h4> Something went wrong </h4>`;
+    }
+
+}
